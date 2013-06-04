c2a6910e40b9e64337fac838849c45532d3b4ebe
Michael Billington <michael.billington@gmail.com>
2013-06-04 12:00:15 +1000

    implement createUser

---
+++ examples/createUser.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+/* Example of creating an account */
+
+require_once("../ProvisioningApi.php");
+
+if(count($argv) < 7) {
+	die("Usage: ". $argv[0] . " admin@example.com password Joe Bloggs joebloggs@example.com (SHA hash of new user password)\nGenerate the password with:\n\techo -n verysecretpassword | sha1sum\n");
+}
+
+/* Admin */
+$username = $argv[1];
+$adminPassword = $argv[2];
+
+/* User details */
+$firstName = $argv[3];
+$lastName = $argv[4];
+$userEmail = $argv[5];
+$password = $argv[6];
+
+try {
+	echo "Logging in ...\n";
+	$prov = new ProvisioningApi($username, $adminPassword);
+	$prov -> createUser($userEmail, $firstName, $lastName, $password);
+} catch(Exception $e) {
+	die("Error: " . $e -> getMessage()."\n");
+}
+
+?>
---
+++ types/Provisioning_DomainUser.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Encapsulate domain user, to track changes, and generate XML to create / modify the user.
+ * 
+ * @author Michael Billington <michael.billington@gmail.com>
+ */
+class Provisioning_DomainUser {
+	private $userEmail;
+	private $firstName;
+	private $lastName;
+	private $password;
+	private $hashFunction;
+	private $isAdmin;
+	
+	/**
+	 * Initialise with given parameters
+	 * 
+	 * @param string $userEmail
+	 * @param string $firstName
+	 * @param string $lastName
+	 * @param string $password Hashed password
+	 * @param string $hashFunction Function used to hash the password
+	 * @param boolean $isAdmin True if this is an admin account
+	 */
+	public function __construct($userEmail, $firstName, $lastName, $password, $hashFunction = "SHA-1", $isAdmin = false) {
+		/* Set variables */
+		$this -> userEmail = $userEmail;
+		$this -> firstName = $firstName;
+		$this -> lastName = $lastName;
+		$this -> password = $password;
+		$this -> hashFunction = $hashFunction;
+		$this -> isAdmin = $isAdmin;
+	}
+	
+	/**
+	 * Return XML to create this account
+	 * https://developers.google.com/google-apps/provisioning/#creating_a_user_for_a_domain
+	 */
+	public function createXML() {
+		return "<atom:entry xmlns:atom='http://www.w3.org/2005/Atom'\n" .
+			"xmlns:apps='http://schemas.google.com/apps/2006'>\n" .
+			"<apps:property name=\"password\" value=\"".ProvisioningApi::escapeXML_Attr($this -> password)."\"/>\n" .
+			"<apps:property name=\"hashFunction\" value=\"".ProvisioningApi::escapeXML_Attr($this -> hashFunction)."\"/>\n" .
+			"<apps:property name=\"userEmail\" value=\"".ProvisioningApi::escapeXML_Attr($this -> userEmail)."\"/>\n" .
+			"<apps:property name=\"firstName\" value=\"".ProvisioningApi::escapeXML_Attr($this -> firstName)."\"/>\n" .
+			"<apps:property name=\"lastName\" value=\"".ProvisioningApi::escapeXML_Attr($this -> lastName)."\"/>\n" .
+			"<apps:property name=\"isAdmin\" value=\"" . ($this -> isAdmin ? 'true' : 'false') . "\"/>\n" .
+			"</atom:entry>";
+	}
+	
+}
